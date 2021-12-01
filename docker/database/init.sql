26cf5f78afb9dc8c1aa74075a8ccb7779c65845e
Mason Fisher <sackzombie10@gmail.com>
2021-12-01 14:14:07 -0500

    Getting data from POST and generating a message.

---
+++ docker/database/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE lampsite;
+USE lampsite;
+CREATE TABLE message (id int, sent_utc timestamp, name tinytext, email tinytext, website tinytext, message text);
--- site/guestbook.php
+++ site/guestbook.php
@@ -8,15 +8,15 @@
   <body>
     <div class="container">
       <h1>Guestbook</h1>
-      <form class="" action="guestbook.php" method="post">
+      <form class="" action="<?php $_PHP_SELF ?>" method="post">
         <label for="name">Name:</label>
         <input type="text" name="name" value="" />
         <br />
         <label for="email">Email:</label>
-        <input type="email" name="email" value="" />
+        <input type="text" name="email" value="" />
         <br />
         <label for="website">Website:</label>
-        <input type="url" name="website" value="" />
+        <input type="text" name="website" value="" />
         <br />
         <label for="message">Message:</label>
         <br />
@@ -27,16 +27,38 @@
 
       </form>
       <?php
-      $link=mysqli_connect("database",$_ENV['MYSQL_USER'],$_ENV['MYSQL_PASSWORD'],$_ENV['MYSQL_DATABASE'], 3306);
+      if (isset($_POST['name']) && isset($_POST['message'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $website = $_POST['website'];
+        $message = $_POST['message'];
 
-      if (mysqli_connect_errno()) {
-        die("MySQL connecttion failed: " . mysqli_connect_error());
-      } else {
-        echo "Connected successfully<br />";
-        echo mysqli_get_server_info($link);
+        $link=mysqli_connect("database",$_ENV['MYSQL_USER'],$_ENV['MYSQL_PASSWORD'],$_ENV['MYSQL_DATABASE'], 3306);
+
+        if (mysqli_connect_errno()) {
+          die("MySQL connecttion failed: " . mysqli_connect_error());
+        } else {
+          echo "Connected successfully<br />";
+          echo mysqli_get_server_info($link);
+
+          echo "<div class=\"guestpost\">";
+          echo "<p>Name: $name</p>";
+          if (isset($email)) {
+            echo "<p>Email: $email</p>";
+          }
+          if (isset($website)) {
+            if (!str_starts_with($website, "http://") && !str_starts_with($website, "https://")) {
+              $website = "https://" . $website;
+            }
+            echo "<p>Website: <a href=\"$website\">$website</a></p>";
+          }
+          echo "<br />";
+          echo "<p>$message</p>";
+          echo "</div>";
+        }
+
+        mysqli_close($link);
       }
-
-      mysqli_close($link);
       ?>
     </div>
   </body>
