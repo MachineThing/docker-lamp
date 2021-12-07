6e73c8e303ff001ec1d1160abb6a0547eaab4f85
Mason Fisher <sackzombie10@gmail.com>
2021-12-07 18:14:19 -0500

    Getting information from database, also updated Dockerfile versions

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,3 +1,4 @@
-CREATE DATABASE lampsite;
-USE lampsite;
-CREATE TABLE message (id int, sent_utc timestamp, name tinytext, email tinytext, website tinytext, message text);
+CREATE TABLE messages (id int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY, sent bigint unsigned NOT NULL, name tinytext NOT NULL, email tinytext, website tinytext, message text NOT NULL);
+
+INSERT INTO messages (sent, name, email, website, message)
+VALUES (0, "Mr. Tester", "test@example.com", "example.com", "If you are reading this then you got the messages correctly!");
--- site/guestbook.php
+++ site/guestbook.php
@@ -27,38 +27,63 @@
 
       </form>
       <?php
+      function guestpost($time, $name, $email, $website, $message) {
+        $strdate = date("l, F jS, Y g:i:s A", $time);
+
+        echo "<div class=\"guestpost\">";
+        echo "<p>Date: $strdate</p>";
+        echo "<p>Name: $name</p>";
+        if ($email != "") {
+          echo "<p>Email: $email</p>";
+        }
+        if ($website != "") {
+          $link_website = null;
+          if (!str_starts_with($website, "http://") && !str_starts_with($website, "https://")) {
+            $link_website = "https://" . $website;
+          } else {
+            $link_website = $website;
+          }
+          echo "<p>Website: <a href=\"$link_website\">$website</a></p>";
+        }
+        echo "<br />";
+        echo "<p>$message</p>";
+        echo "</div>";
+        echo "<br />";
+      }
+
+      // Connect to database
+      $link=mysqli_connect("database",$_ENV['MYSQL_USER'],$_ENV['MYSQL_PASSWORD'],$_ENV['MYSQL_DATABASE'], 3306);
+
+      if (mysqli_connect_errno()) {
+        die("MySQL connecttion failed: " . mysqli_connect_error());
+      }
+
+      // On POST request
+
       if (isset($_POST['name']) && isset($_POST['message'])) {
+        $time = time();
         $name = $_POST['name'];
         $email = $_POST['email'];
         $website = $_POST['website'];
         $message = $_POST['message'];
 
-        $link=mysqli_connect("database",$_ENV['MYSQL_USER'],$_ENV['MYSQL_PASSWORD'],$_ENV['MYSQL_DATABASE'], 3306);
-
-        if (mysqli_connect_errno()) {
-          die("MySQL connecttion failed: " . mysqli_connect_error());
-        } else {
-          echo "Connected successfully<br />";
-          echo mysqli_get_server_info($link);
-
-          echo "<div class=\"guestpost\">";
-          echo "<p>Name: $name</p>";
-          if (isset($email)) {
-            echo "<p>Email: $email</p>";
-          }
-          if (isset($website)) {
-            if (!str_starts_with($website, "http://") && !str_starts_with($website, "https://")) {
-              $website = "https://" . $website;
-            }
-            echo "<p>Website: <a href=\"$website\">$website</a></p>";
-          }
-          echo "<br />";
-          echo "<p>$message</p>";
-          echo "</div>";
-        }
-
-        mysqli_close($link);
+        guestpost($time, $name, $email, $website, $message);
       }
+
+      // Get all posts
+
+      $sql = "SELECT sent, name, email, website, message FROM messages ORDER BY id";
+      $result = $link->query($sql);
+
+      if (mysqli_num_rows($result) > 0) {
+        // Output data of all rows
+        while ($row = mysqli_fetch_assoc($result)) {
+          guestpost($row['sent'], $row['name'], $row['email'], $row['website'], $row['message']);
+        }
+      }
+
+      // Done
+      mysqli_close($link);
       ?>
     </div>
   </body>
