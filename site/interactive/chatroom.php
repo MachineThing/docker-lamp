04df2ad9e9e0d4b9bd704a7d33972b5515dab372
Mason Fisher <sackzombie10@gmail.com>
2021-12-22 20:30:01 -0500

    Made chatroom site not a test ground

--- site/interactive/chatroom.php
+++ site/interactive/chatroom.php
@@ -3,28 +3,69 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/css/index.css">
+    <link rel="stylesheet" href="/css/chatroom.css">
     <title>My Site!</title>
   </head>
   <body>
     <div class="container">
       <h1>Chatroom</h1>
-      <p><?php
-      $str_to_send = "SEND: Nick: Hello, world!";
-
-      $port_number = 3001;
-      $ip_address = "chatroom";
-
-      $sock = socket_create(AF_INET, SOCK_STREAM, 0) or die("Unable to create connection with socket\n");
-      $server = socket_connect($sock, $ip_address, $port_number) or die("Unable to create connection with server\n");
-
-      socket_write($sock, $str_to_send, strlen($str_to_send)) or die("Unable to send data to the server\n");
-
-      $server_recv = socket_read($sock, 1024) or die("Unable to read response from the server\n");
-
-      echo $server_recv;
-
-      socket_close($sock);
-      ?></p>
+      <div class="chat_head">
+        <p>Hello, Mason!</p>
+      </div>
+      <div class="chat">
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+        <p>Mason: Howdy</p>
+      </div>
+      <div class="chat_foot">
+        <input type="text" name="" value="">
+      </div>
     </div>
   </body>
 </html>
